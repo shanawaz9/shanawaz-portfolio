f0a78028945f22322964ecd19e2f0eb15aa2e57b
shanawazdesign <shaikshanawazh@aapmor.com>
2026-07-14 15:26:00 +0530

    resume update

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -6,7 +6,7 @@ const CYCLE_WORDS = ['Curiosity', 'Empathy', 'Intent'];
 const SCRAMBLE_CHARS = '◆○□△◇●■▲◈◉◎·—∙⬡✦◐◑';
 
 const RESUME_URL =
-  'https://drive.google.com/file/d/1ykbOksFk2TVq_DqjcCJnp0pBTNzEd-_X/view?usp=sharing';
+  'https://drive.google.com/file/d/1weBFGYehRwqWccuizf_Yzl5hv2rZWRnH/view?usp=sharing';
 const EMAIL_ADDRESS = 'shanawazhussain989@gmail.com';
 const FLOATING_WINDOWS = [
   {
